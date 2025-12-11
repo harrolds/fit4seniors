35eb13a6375a475ac228ba8cfa3864c7be7c76c0
Harrold <36454763+harrolds@users.noreply.github.com>
2025-12-11 15:32:34 +0100

    Afgerond - Fase 5 — Fit4Seniors UI-Kit & Brand Lock-in

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q56kr0fi0h"
+    "revision": "0.sg4297q35a"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
